2f1951b199ba571dc28ee5402d8a1d65206d3981
townageriron@gmail.com <townageriron@gmail.com>
2024-12-18 23:33:32 +0900

    nginx経由でadminログイン

--- django_nginx/django_app/config/settings.py
+++ django_nginx/django_app/config/settings.py
@@ -122,5 +122,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-
-ALLOWED_HOSTS = ["host.docker.internal"]
+USE_X_FORWARDED_HOST = True
+ALLOWED_HOSTS = ["localhost"]
