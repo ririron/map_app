a243417250599b1d6981fe640da80d7a8399eb3a
townageriron@gmail.com <townageriron@gmail.com>
2025-01-19 23:19:29 +0900

    filterで変なことをしていたので修正

--- django_nginx/django_app/task_log/filters.py
+++ django_nginx/django_app/task_log/filters.py
@@ -1,29 +1,7 @@
-import django_filters
+from django_filters import rest_framework as filters
 from .models import UserTask, TaskLog
 
-class UserTaskFilter(django_filters.FilterSet):
-
-	def __init__(self, *args, **kwargs):
-		self.request = kwargs.pop('request', None)  # requestオブジェクトを取得
-		super().__init__(*args, **kwargs)
-
-	def filter_queryset(self, queryset):
-		if self.request:
-			# ログインユーザーの投稿のみを取得
-			queryset = queryset.filter(task_owner=self.request.user.id)
-		return super().filter_queryset(queryset)
-
-class TaskLogFilter(django_filters.FilterSet):
+class TaskLogFilter(filters.FilterSet):
 	class Meta:
 		model = TaskLog
 		fields = ["task_id"]
-
-	def __init__(self, *args, **kwargs):
-		self.request = kwargs.pop('request', None)  # requestオブジェクトを取得
-		super().__init__(*args, **kwargs)
-
-	def filter_queryset(self, queryset):
-		if self.request:
-			# ログインユーザーの投稿のみを取得
-			queryset = queryset.filter(task_owner=self.request.user.id)
-		return super().filter_queryset(queryset)
--- django_nginx/django_app/task_log/views.py
+++ django_nginx/django_app/task_log/views.py
@@ -2,19 +2,21 @@ from rest_framework.generics import ListAPIView
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.permissions import AllowAny,IsAuthenticated
 from .models import UserTask, TaskLog
-from .filters import UserTaskFilter, TaskLogFilter
+from .filters import TaskLogFilter
 from .serializers import UserTaskSerializer, TaskLogSerializer
 # Create your views here.
 
 class UserTaskListView(ListAPIView):
-    queryset = UserTask.objects.all()
     permission_classes = [IsAuthenticated]
     serializer_class = UserTaskSerializer
-    filterset_class = UserTaskFilter
 
+    def get_queryset(self):
+        return UserTask.objects.filter(task_owner=self.request.user)
 
 class TaskLogListView(ListAPIView):
-    queryset = TaskLog.objects.all()
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticated]
     serializer_class = TaskLogSerializer
     filterset_class = TaskLogFilter
+
+    def get_queryset(self):
+        return TaskLog.objects.filter(task_owner=self.request.user)
